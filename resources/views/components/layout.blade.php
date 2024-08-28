d7019f84eea260789426c4443f0be4e0cf3c758a
ciprian <ciprianvlad888@gmail.com>
2024-08-28 06:25:40 +0100

    styled job-cards

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Pixel Positions</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
@@ -25,7 +28,7 @@
                 <a href="">Post a job</a>
             </div>
         </nav>
-
+        
         <main class="mt-10 max-w-[968px] mx-auto">
             {{ $slot }}
         </main>
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,15 @@
-<a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">{{ $slot }}</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/10 rounded-xl hover:bg-white/25 transition-colors duration-300 font-bold";
+
+    if ($size === 'base') {
+        $classes .= "px-5 py-1 text-sm";
+    }
+
+    if ($size === 'small') {
+        $classes .= "px-2 py-1 text-2xs";
+    }
+@endphp
+
+<a href="#" class="{{ $classes }}">{{ $slot }}</a>
