7c24b84d89712b9b56d5d4fde815f8152c1287c4
ciprian <ciprianvlad888@gmail.com>
2024-08-29 06:05:15 +0100

    added tags to db

--- database/migrations/2024_08_28_191735_create_jobs_table.php
+++ database/migrations/2024_08_28_191735_create_jobs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\Employer;
 return new class extends Migration
 {
     /**
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Employer::class); // Foreign key to employers table
+            $table->string('title');
+            $table->decimal('salary', 8, 2); // Adjust the precision as needed
+            $table->string('location');
+            $table->string('schedule')->nullable(); // Nullable if it's optional
+            $table->string('url')->nullable(); // Nullable if it's optional
+            $table->boolean('featured')->default(false);
             $table->timestamps();
         });
     }
