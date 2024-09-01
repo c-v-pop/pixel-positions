8708f328cc5d97ae25c484aafd490681578319bc
ciprian <ciprianvlad888@gmail.com>
2024-09-01 20:29:59 +0100

    created routes for session controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register',[RegisteredUserController::class,'create']);
+Route::get('/register',[RegisteredUserController::class,'store']);
+
+Route::get('/login', [SessionController::class,'create']);
+Route::post('/login', [SessionController::class,'store']);
+Route::delete('/logout', [SessionController::class,'destroy']);
