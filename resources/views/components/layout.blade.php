e9c74dc67c01ebab8c69ba91af81686fd6c8a529
Vlad Pop <61190539+c-v-pop@users.noreply.github.com>
2024-10-03 05:31:45 +0100

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,13 +11,42 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-black text-white font-hanken-grotesk pb-20">
-    <div class="flex justify-center items-center">
-        <h1 class="text-red-500">Pixel Positions</h1>
-        <ul>
-            <h2>This will be a job board to:</h2>
-            <li>Post a job</li>
-            <li>Find a job</li>
-            <li>Filter through jobs</li>
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+            <div class="space-x-6 font-bold">
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            
+            @auth
+                <div class="space-x-6 font-bold flex">
+                   <a href="/jobs/create">Post a job</a>
+
+                   <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Logout</button>
+                </form>
+                </div>
+            @endauth
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Login</a>
+                </div>
+            @endguest
+        </nav>
+        
+        <main class="mt-10 max-w-[968px] mx-auto">
+            {{ $slot }}
+        </main>
     </div>
 </body>
 </html>
